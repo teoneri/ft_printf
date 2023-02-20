6d3f1111466d1b68adc961cb98841c19b6f3d03f
Matteo Neri <mneri@c1r3p11.42firenze.it>
2023-02-20 12:11:04 +0100

    fix putunsignednbr

--- SRC/ft_putunsignednbr.c
+++ SRC/ft_putunsignednbr.c
@@ -14,19 +14,7 @@
 
 void	ft_putunsignednbr(unsigned int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		ft_putunsignednbr(147483648, fd);
-	}
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-		ft_putunsignednbr(n, fd);
-	}
-	else if (n > 9)
+	if (n > 9)
 	{
 		ft_putunsignednbr(n / 10, fd);
 		ft_putunsignednbr(n % 10, fd);
